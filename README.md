5662792939373bcc6cd68bad763b78bce6d60058
sergei-doroshenko <6823298@gmail.com>
2019-02-02 10:42:06 +0300

    simple web server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 $ go install github.com/go_samples
+
+http://localhost:8080/honey
--- main.go
+++ main.go
@@ -7,12 +7,10 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Fatal("Got somethis")
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	log.Fatal("starting...")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
